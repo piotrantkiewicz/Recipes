b4c1e18eaf876b43a950a1f779704ac62d6100c3
Piotr Antkiewicz <55496699+piotrantkiewicz@users.noreply.github.com>
2024-11-12 15:18:27 +0100

    add RecipeCell

--- Recipes/Presentation/Home/HomeViewController.swift
+++ Recipes/Presentation/Home/HomeViewController.swift
@@ -4,6 +4,27 @@ class HomeViewController: ViewController {
     
     private let viewModel: HomeViewModel
     
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(RecipeCell.self, forCellReuseIdentifier: "RecipeCell")
+        tableView.dataSource = self
+        //        tableView.delegate = self
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .background
+        return tableView
+    }()
+    
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.placeholder = "search.placeholder".localized
+        //        searchBar.delegate = self
+        searchBar.backgroundImage = UIImage()
+        searchBar.backgroundColor = .clear
+        return searchBar
+    }()
+    
     init(viewModel: HomeViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -16,12 +37,31 @@ class HomeViewController: ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadRecipes()
+        setupUI()
+        configureDismissKeyboardGesture()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .background
+        view.addSubview(tableView)
+        view.addSubview(searchBar)
+        
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 16),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
     }
     
     private func loadRecipes() {
         Task {
             await viewModel.loadRecipes()
-            // tableView.reloadData()
+            tableView.reloadData()
         }
     }
     
@@ -29,5 +69,31 @@ class HomeViewController: ViewController {
         viewModel.showError = { [weak self] error in
             self?.showErrorAlert(message: error)
         }
+    }
+    
+    private func configureDismissKeyboardGesture() {
+        let tapGesture = UITapGestureRecognizer(
+            target: self,
+            action: #selector(dismissKeyboard)
+        )
+        tapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func dismissKeyboard() {
+        searchBar.resignFirstResponder()
+    }
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.recipes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath) as! RecipeCell
+        let recipe = viewModel.recipes[indexPath.row]
+        cell.configure(with: recipe)
+        return cell
     }
 }
